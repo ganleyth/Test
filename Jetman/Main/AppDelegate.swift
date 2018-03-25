c3e8c6fc5e9d0e270a26924b97aed7cc078acdc0
Tom <ganleyapps@gmail.com>
2018-03-25 16:43:42 -0700

    Add the sign up vc and a segue to it from the landing view.

--- Jetman/Main/AppDelegate.swift
+++ Jetman/Main/AppDelegate.swift
@@ -20,8 +20,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        autheticateLocalPlayer()
 
         // Load sprites into memory on launch
         SpriteLoader.shared.loadSprites(for: GameSession.shared.settings.playerGender)
