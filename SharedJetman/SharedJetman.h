084af953879854a90abfbe097b0b94870ff58593
Tom <ganleyapps@gmail.com>
2018-04-08 19:54:50 -0700

    Create the shared framework and move shared assets to the shared framework.

---
+++ SharedJetman/SharedJetman.h
@@ -0,0 +1,19 @@
+//
+//  SharedJetman.h
+//  SharedJetman
+//
+//  Created by Thomas Ganley on 4/8/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SharedJetman.
+FOUNDATION_EXPORT double SharedJetmanVersionNumber;
+
+//! Project version string for SharedJetman.
+FOUNDATION_EXPORT const unsigned char SharedJetmanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SharedJetman/PublicHeader.h>
+
+
