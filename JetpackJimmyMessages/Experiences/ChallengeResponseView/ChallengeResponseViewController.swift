59dbe9918a557b1d1b8223744c24b5ae5a1bd72f
Tom <ganleyapps@gmail.com>
2018-04-15 08:57:06 -0700

    Add storyboard and view controller files for the default compact view and the challenge response view.

---
+++ JetpackJimmyMessages/Experiences/ChallengeResponseView/ChallengeResponseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChallengeResponseViewController.swift
+//  JetpackJimmyMessages
+//
+//  Created by Thomas Ganley on 4/15/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeResponseViewController: UIViewController {
+    
+    @IBAction func acceptChallenge(_ sender: UIButton) {
+    }
+    
+    @IBAction func declineChallenge(_ sender: UIButton) {
+    }
+}
